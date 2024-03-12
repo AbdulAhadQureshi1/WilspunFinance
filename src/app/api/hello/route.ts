086aff7a213842f969aaa610ffd6120b99083a33
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-03-12 14:10:31 +0500

    add testing api

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: NextApiRequest,
+) {
+    return NextResponse.json("ok")
+}
